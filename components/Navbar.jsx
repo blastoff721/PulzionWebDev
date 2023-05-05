fceeae983e2250ffa59ab114d324ac9b67ed2ead
Aaryaman Limaye <limayeaaryaman7@gmail.com>
2023-05-05 14:07:22 +0530

    added final2

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -7,7 +7,6 @@ import { navVariants } from '../utils/motion';
 const Navbar = () => (
   <motion.nav
     variants={navVariants}
-    initial="hidden"
     whileInView="show"
     className={`${styles.xPaddings} py-8 relative`}
   >
